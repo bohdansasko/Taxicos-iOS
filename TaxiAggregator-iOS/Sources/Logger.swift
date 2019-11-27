24016634eb2b6ff464eb66c8bc3182f0d3d48ac4
bohdans <0000xffff@gmail.com>
2019-11-27 23:40:02 +0200

    TA-13: Make screen "Search results" - improve logger

--- TaxiAggregator-iOS/Sources/Logger.swift
+++ TaxiAggregator-iOS/Sources/Logger.swift
@@ -12,12 +12,12 @@ struct log {
     
     private init() {}
     
-    private static func log(level: Level, _ items: [Any], pathToFile: String, line: Int) {
+    private static func log(level: Level, _ items: [Any], pathToFile: String, line: Int, function: String) {
         #if DEBUG
             let fileName = (pathToFile as NSString).lastPathComponent
             let argsAsStr = items.map { String(describing: $0) }.joined(separator: " ")
             let threadAsStr = Thread.current.isMainThread ? "thread: <main>" : "thread: <background>"
-            print(level.icon, Date().now, threadAsStr, line, fileName, argsAsStr)
+            print(level.icon, Date().now, threadAsStr, line, fileName, function, argsAsStr)
         #endif
     }
 
@@ -47,20 +47,20 @@ private extension log {
 
 extension log {
     
-    static func debug(_ message: Any..., pathToFile: String = #file, line: Int = #line) {
-        log(level: .debug, message, pathToFile: pathToFile, line: line)
+    static func debug(_ message: Any..., pathToFile: String = #file, line: Int = #line, function: String = #function) {
+        log(level: .debug, message, pathToFile: pathToFile, line: line, function: function)
     }
     
-    static func info(_ message: Any..., pathToFile: String = #file, line: Int = #line) {
-        log(level: .info, message, pathToFile: pathToFile, line: line)
+    static func info(_ message: Any..., pathToFile: String = #file, line: Int = #line, function: String = #function) {
+        log(level: .info, message, pathToFile: pathToFile, line: line, function: function)
     }
     
-    static func error(_ message: Any..., pathToFile: String = #file, line: Int = #line) {
-        log(level: .error, message, pathToFile: pathToFile, line: line)
+    static func error(_ message: Any..., pathToFile: String = #file, line: Int = #line, function: String = #function) {
+        log(level: .error, message, pathToFile: pathToFile, line: line, function: function)
     }
     
-    static func network(_ message: Any..., pathToFile: String = #file, line: Int = #line) {
-        log(level: .network, message, pathToFile: pathToFile, line: line)
+    static func network(_ message: Any..., pathToFile: String = #file, line: Int = #line, function: String = #function) {
+        log(level: .network, message, pathToFile: pathToFile, line: line, function: function)
     }
     
 }
